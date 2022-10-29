93da0f2f13732929de09b94ad0b19f54a62b522f
Eseoghene Okoro <enerhen2013@gmail.com>
2022-10-28 22:25:43 -0700

    test

--- Program.cs
+++ Program.cs
@@ -5,12 +5,46 @@ using ServerTask.Helper;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
-var defaultConnectionString = builder.Configuration.GetConnectionString(ConnectionHelper.BuildConnectionString(databaseUrl));
+var defaultConnectionString = string.Empty;
+
+if (builder.Environment.EnvironmentName == "Development") {
+    defaultConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+}
+else
+{
+    // Use connection string provided at runtime.
+    var connectionUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+    connectionUrl = connectionUrl.Replace("postgres://", string.Empty);
+    var userPassSide = connectionUrl.Split("@")[0];
+    var hostSide = connectionUrl.Split("@")[1];
+
+    var user = userPassSide.Split(":")[0];
+    var password = userPassSide.Split(":")[1];
+    var host = hostSide.Split("/")[0];
+    var database = hostSide.Split("/")[1].Split("?")[0];
+
+    defaultConnectionString = $"Host={host};Database={database};Username={user};Password={password};SSL Mode=Require;Trust Server Certificate=true";
+}
+
+
+
+
+// var defaultConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<AppDbContext>(options => {
     options.UseNpgsql(defaultConnectionString);
 });
+
+var serviceProvider = builder.Services.BuildServiceProvider();
+try
+{
+    var dbContext = serviceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
+}
+catch
+{
+}
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -19,8 +53,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-var scope = app.Services.CreateScope();
-await DataHelper.ManageDataAsync(scope.ServiceProvider);
+// var scope = app.Services.CreateScope();
+// await DataHelper.ManageDataAsync(scope.ServiceProvider);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
